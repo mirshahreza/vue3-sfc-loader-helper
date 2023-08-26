a8df3476cd7f2785f4a55c6f510d927b5ef8cee2
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-08-26 09:54:21 +0330

    Add project files.

---
+++ components/one.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 >Component one : {{ title }}</h1>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            title: {
+                type: String,
+                required: true,
+                default: 'Title One'
+            }
+        },
+        setup(props) {
+            //console.log(props.test)
+        }
+    }
+
+</script>
---
+++ components/two.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 >Component two : {{ title }}</h1>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            title: {
+                type: String,
+                required: true,
+                default: 'Title Tow'
+            }
+        },
+        setup(props) {
+            console.log(props.test)
+        }
+    }
+
+</script>
